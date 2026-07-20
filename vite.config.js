4827adfbe47b6337e2d6ca11b9b3139ce7ff93e6
shyam kasliwal <shy9kasliwal@gmail.com>
2026-07-20 07:44:30 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import federation from '@originjs/vite-plugin-federation';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const remoteUrl =
+    env.VITE_MFE_REMOTE_URL || 'http://localhost:5001/assets/remoteEntry.js';
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      federation({
+        name: 'host',
+        remotes: {
+          musicLibrary: remoteUrl,
+        },
+        shared: ['react', 'react-dom', '@tanstack/react-query'],
+      }),
+    ],
+    optimizeDeps: {
+      exclude: ['musicLibrary/MusicLibrary'],
+    },
+    build: {
+      modulePreload: false,
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false,
+    },
+    server: {
+      port: 5000,
+      strictPort: true,
+    },
+    preview: {
+      port: 5000,
+      strictPort: true,
+    },
+  };
+});
